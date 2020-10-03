c75f3aa535795e9424da889bcdde679fb4b58ff6
Artur Mendes <arturmendessantos@hotmail.com>
2020-10-03 14:29:20 -0300

    commit

--- ManipulaArqTexto/src/util/GravarArqTexto.java
+++ ManipulaArqTexto/src/util/GravarArqTexto.java
@@ -4,9 +4,7 @@ package util;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 public class GravarArqTexto {
 
---
+++ ManipulaArqTexto/src/util/ManipulaArquivoTXT.java
@@ -0,0 +1,69 @@
+package util;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.swing.JOptionPane;
+
+import editorTexto.JanelaPrincipal;
+
+public class ManipulaArquivoTXT {
+
+	public static boolean gravarArquivoTXT(String nomeArquivo, String conteudo, JanelaPrincipal janelaPrincipal) {
+		try {
+
+			System.out.println("Abrindo para gravação teste.txt");
+
+			FileWriter fout = new FileWriter(nomeArquivo);
+			PrintWriter pout = new PrintWriter(fout);
+
+			System.out.println("Gravando teste.txt");
+			pout.println(conteudo);
+
+			System.out.println("Fechando teste.txt");
+			pout.close();
+			fout.close();
+
+			return true;
+
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(janelaPrincipal, "Erro: " + e.getMessage());
+			e.printStackTrace();
+		}
+
+		return false;
+	}
+
+	public static String lerArquivoTXT(String nomeArquivo, JanelaPrincipal janelaPrincipal) {
+		try {
+
+			FileReader fin = new FileReader(nomeArquivo);
+			BufferedReader bin = new BufferedReader(fin);
+
+			String linha = bin.readLine();
+			String retorno = "";
+			while (linha != null) {
+				
+				retorno += linha + "\n";
+				
+				linha = bin.readLine();
+			}
+
+			bin.close();
+			fin.close();
+
+			JOptionPane.showMessageDialog(janelaPrincipal, "Arquivo Aberto!!!");
+			
+			return retorno;
+
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(janelaPrincipal, "Erro: " + e.getMessage());
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
